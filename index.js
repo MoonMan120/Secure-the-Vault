3e7e5ff39d294072732bf542f1618b7675335a88
Andre Moon <moonman120@gmail.com>
2025-07-23 11:47:21 -0500

    code added

---
+++ index.js
@@ -0,0 +1,16 @@
+let code1;
+
+code1 = 7 + 3;
+
+let code2 = (100 - 80) * 2;
+
+let code3 = 76 / 2 + (5 % 4);
+
+let message = "The vault has been secured. The combination is";
+
+let codeA = code1 + "-" + code2 + "-" + code3;
+let codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
